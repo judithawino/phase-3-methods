33e119eccb68c822475f7138f68f1a8553f109f0
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-16 23:28:23 +0300

    commit tree 99409d0cba901abdfabfba8370d20aa383c3fdbb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+greet_programmer 
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen") 
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default ()
+
+def add(num1, num2)
+    return num1 + num2
+end 
+
+def halve (num1)
+    if num1.class != Integer
+        return nil
+    end 
+    num1 / 2
+end 
+halve (6)      
